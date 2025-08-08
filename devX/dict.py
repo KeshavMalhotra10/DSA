648510e5549f44c2cc700605eebc94bd297ca033
Keshav Malhotra <malhotrakeshav10@gmail.com>
2025-08-07 20:42:22 -0400

    freecode commit

---
+++ devX/dict.py
@@ -0,0 +1,15 @@
+# dictionaries
+
+"""
+key value pairs
+associative array, HashMap
+dicts are unordered
+"""
+
+# constructors
+x = {"pork": 25.3, "beef": 33.8, "chicken": 22.7}
+print(x)
+
+
+x = dict([("pork", 25.3), ("beef", 33.8), ("chicken", 22.7)])
+print(x)
