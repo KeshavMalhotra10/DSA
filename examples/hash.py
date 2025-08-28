3bb2cc4719e22a84d60bf28500d3297c4ab2f212
Keshav Malhotra <malhotrakeshav10@gmail.com>
2025-08-28 16:45:28 -0230

    I added the sudoku leet code problem utilizing hashset data retrieval

--- examples/hash.py
+++ examples/hash.py
@@ -49,7 +49,9 @@ fast lookup
 # 2 populate hash map with each number and corresponding index
 # 3. check 1 to 2
 
-
+# solution
+# --------
+"""
 def pair_sum_unsorted_two_pass(nums, target):
     hash_map = {}  # store num and indice
     for i, num in enumerate(nums):
@@ -57,3 +59,57 @@ def pair_sum_unsorted_two_pass(nums, target):
         if needed in hash_map:
             return [hash_map[needed], i]
         hash_map[num] = i
+"""
+
+# Verify Sudoku Board
+"""
+Given a partially completed 9x9 Sudoku board, determine if the current state of the board
+adheres to the rules of the game:
+• Each row and column must contain unique numbers between 1 and 9, or be empty
+(represented as 0).
+• Each of the nine 3x3 subgrids that compose the grid must contain unique numbers
+between 1 and 9, or be empty.
+Note: You are asked to determine whether the current state of the board is valid given
+these rules, not whether the board is solvable.
+
+
+"""
+
+# solution
+# --------
+# 1. check whether row is unique
+# 2. check whether column is unique
+# 3. view the sudoku board as a 3x3 column such that rows and columns are 0,1,2
+# 4. if ever the board becomes non-unique, return False
+
+
+def verify_board(board):
+    # create has shets for each row, column, and subgrid to keep track
+    # of numbers previously seen on any given row, column, or subgrid
+    row_sets = [
+        set() for _ in range(9)
+    ]  # note I use _ instead of a character, we do this to say arbritary variable not used further
+    column_sets = [set() for _ in range(9)]
+    subgrid_sets = [[set() for _ in range(3)] for _ in range(3)]
+
+    for r in range(9):
+        for c in range(9):
+            num = board[r][c]
+            if num == 0:
+                continue
+            # check if num has been seen in current row, column, or subgrid
+            if num in row_sets[r]:
+                return False
+            if num in column_sets[c]:
+                return False
+            if (
+                num in subgrid_sets[r // 3][c // 3]
+            ):  # we do //3 because we made a 9x9 into a 3x3
+                return False
+
+            # if we passed above checks, mark value as seen and add it to
+            # corresponding hashsets
+            row_sets[r].add(num)
+            column_sets(c).add(num)
+            subgrid_sets[r // 3][c // 3].add(num)
+        return True
